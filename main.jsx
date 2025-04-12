1822c32bbbdc2fe7fc5032dbd4f66f00a9bdb497
soyKikeangola <90267817+soyKikeangola@users.noreply.github.com>
2025-04-12 16:10:17 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CalculadoraTimbres from "./CalculadoraTimbres";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CalculadoraTimbres />
+  </React.StrictMode>
+);
